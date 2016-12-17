4eddae3f662687b6c194e240b9aa5a4f25a8698f
Ariel Henryson <ariel@qwikwiz.com>
2016-12-17 15:46:56 +0200

    fix

--- src/core/webpack.config.dev.js
+++ src/core/webpack.config.dev.js
@@ -18,10 +18,5 @@ module.exports = {
     },
     resolve: {
         extensions: ['', '.js', '.ts']
-    },
-    module: {
-        loaders: [{
-            test: /\.ts/, loaders: ['ts-loader'], exclude: /node_modules/
-        }]
     }
 };
--- src/core/webpack.config.js
+++ src/core/webpack.config.js
@@ -23,10 +23,5 @@ module.exports = {
     },
     resolve: {
         extensions: ['', '.js', '.ts']
-    },
-    module: {
-        loaders: [{
-            test: /\.ts/, loaders: ['ts-loader'], exclude: /node_modules/
-        }]
     }
 };
