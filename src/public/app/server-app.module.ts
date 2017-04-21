e56d19c501ae2afef9ad561d7310ec90e230be77
Ariel Henryson <ariel@qwikwiz.com>
2017-04-21 12:59:05 +0300

    Preparations for migration to Angular 4

--- src/public/app/server-app.module.ts
+++ src/public/app/server-app.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { UniversalModule } from 'angular2-universal';
-import { RouterModule } from "@angular/router";
-
 import {App} from './app.component';
-import {Home, Page2, Page3, _routes} from "./app.routes";
-import {MainModule} from "./main.module";
+import { MainModule } from "./main.module";
 
 @NgModule({
     bootstrap: [ App ],
