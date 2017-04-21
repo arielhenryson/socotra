2fe9e7b30f61aeb62cb6b39e32fc09e97c1b0afd
Ariel Henryson <ariel@qwikwiz.com>
2017-04-21 14:31:18 +0300

    Preparations for migration to Angular 4

--- src/core/angularUniversal/__workaround.node.ts
+++ src/core/angularUniversal/__workaround.node.ts
@@ -1,8 +1,3 @@
-/*
- * THIS IS TEMPORARY TO PATCH 2.1.1+ Core bugs
- */
-
-/* tslint:disable */
 let __compiler__ = require('@angular/compiler');
 import { __platform_browser_private__ } from '@angular/platform-browser';
 import { __core_private__ } from '@angular/core';
@@ -19,25 +14,24 @@ if (__compiler__ && __compiler__.SelectorMatcher && __compiler__.CssSelector) {
     (__compiler__).__compiler_private__ = {
         SelectorMatcher: __compiler__.SelectorMatcher,
         CssSelector: __compiler__.CssSelector
-    }
+    };
 }
 
 if (patch) {
-    var __universal__ = require('angular2-platform-node/__private_imports__');
+    let __universal__ = require('angular2-platform-node/__private_imports__');
     __universal__.ViewUtils = __core_private__['view_utils'];
     __universal__.CssSelector = __universal__.CssSelector || __compiler__.CssSelector;
     __universal__.SelectorMatcher = __universal__.SelectorMatcher || __compiler__.SelectorMatcher;
 }
 
-// Fix Material Support
+
 function universalMaterialSupports(eventName: string): boolean { return Boolean(this.isCustomEvent(eventName)); }
 __platform_browser_private__.HammerGesturesPlugin.prototype.supports = universalMaterialSupports;
-// End Fix Material Support
 
-// Fix Universal Style
+
+
 import { NodeDomRootRenderer, NodeDomRenderer } from 'angular2-universal/node';
 function renderComponentFix(componentProto: any) {
     return new NodeDomRenderer(this, componentProto, this._animationDriver);
 }
 NodeDomRootRenderer.prototype.renderComponent = renderComponentFix;
-// End Fix Universal Style
