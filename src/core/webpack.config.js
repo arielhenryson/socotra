6e8ccf9f1ac03e01969ba96184efe360f92684bb
Ariel Henryson <ariel@qwikwiz.com>
2017-04-22 14:23:44 +0300

    migration to Angular 4

--- src/core/webpack.config.js
+++ src/core/webpack.config.js
@@ -2,33 +2,55 @@ const path = require("path");
 const ROOT = "../../";
 const webpack = require('webpack');
 
+const { AotPlugin } = require('@ngtools/webpack');
+
+
+const useAOT = false;
+
+
+let tsLoader = {
+        test: /\.ts/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
+        exclude: /node_modules/
+};
+
+let plugins = [];
+if (useAOT) {
+        const aot = new AotPlugin({
+                tsConfigPath: path.normalize(ROOT + "./tsconfig.json")
+                //entryModule:  "../../src/public/app/browser-app.module#BrowserAppModule"
+        });
+        plugins.push(aot);
+        tsLoader = {
+                test: /\.ts$/,
+                loader: '@ngtools/webpack',
+        };
+}
+
 
 module.exports = {
-    entry: {
+        entry: {
         app: path.normalize(ROOT + "./src/public/app/main.browser")
-    },
-    output: {
-        path: __dirname,
-        filename: path.normalize(ROOT +"./.build/public/dist/[name].js")
-    },
-    resolve: {
-        extensions: [ '.ts', '.js']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.ts/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(html|css)$/,
-                loader: 'raw-loader'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'null'
-            },
-        ]
-    }
+        },
+        output: {
+                path: __dirname,
+                filename: path.normalize(ROOT +"./.build/public/dist/[name].js")
+        },
+                resolve: {
+                extensions: [ '.ts', '.js']
+        },
+        module: {
+                loaders: [
+                        tsLoader,
+                        {
+                                test: /\.(html|css)$/,
+                                loader: 'raw-loader'
+                        },
+                        {
+                                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                                loader: 'null'
+                        },
+                ]
+        },
+        plugins: plugins
 };
--- src/public/app/app.component.ts
+++ src/public/app/app.component.ts
@@ -1,42 +1,43 @@
 import {Component} from '@angular/core';
-import RXBox from "rxbox";
+// import RXBox from "rxbox";
 
 @Component({
     selector: 'app',
     templateUrl: 'app.component.html',
-    providers: [RXBox]
+    // providers: [RXBox]
 })
 export class App {
-    constructor(private _store: RXBox) {
-        if (typeof window === "undefined") return;
-        this._store.debug = true;
-
-        this._store.watch('foo.bar').subscribe((val) => {
-            console.log("change in foo.bar value",  val);
-        });
-
-
-        this._store.assignState({foo: {
-            bar: {
-                foo: 1
-            }
-        }});
-
-        this._store.assignState({foo: {
-            bar: {
-                foo: 3333
-            }
-        }});
-
-
-        let index = 0;
-        setInterval(() => {
-            let foo = this._store.getState()['foo'];
-            foo.bar = index;
-            index++;
-            // foo.bar
-            this._store.assignState({foo: foo});
-        }, 1000 * 3);
+    // constructor(private _store: RXBox) {
+    constructor() {
+        // if (typeof window === "undefined") return;
+        // this._store.debug = true;
+        //
+        // this._store.watch('foo.bar').subscribe((val) => {
+        //     console.log("change in foo.bar value",  val);
+        // });
+        //
+        //
+        // this._store.assignState({foo: {
+        //     bar: {
+        //         foo: 1
+        //     }
+        // }});
+        //
+        // this._store.assignState({foo: {
+        //     bar: {
+        //         foo: 3333
+        //     }
+        // }});
+        //
+        //
+        // let index = 0;
+        // setInterval(() => {
+        //     let foo = this._store.getState()['foo'];
+        //     foo.bar = index;
+        //     index++;
+        //     // foo.bar
+        //     this._store.assignState({foo: foo});
+        // }, 1000 * 3);
     }
 
     public testValue = "test";
