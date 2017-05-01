dbf5e4145c20ed0c8907491f47e11ecef3310ace
Ariel Henryson <ariel@qwikwiz.com>
2017-05-01 08:26:46 +0300

    add RequestService

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,39 +1,39 @@
-export const routes = [
+export const routes: Array<SocotraRoute> = [
   {
-    "path": "/",
-    "method": "GET",
-    "controller": "index",
-    "middlewares": ["auth"]
+    path: "/",
+    method: "GET",
+    controller: "index",
+    middlewares: ["auth"]
   },
   {
-    "path": "/unsubscribe/:userID",
-    "controller": "unsubscribe",
-    "middlewares": []
+    path: "/unsubscribe/:userID",
+    controller: "unsubscribe",
+    middlewares: []
   },
   {
-    "path": "/activation/:activationKey",
-    "controller": "activation",
-    "middlewares": []
+    path: "/activation/:activationKey",
+    controller: "activation",
+    middlewares: []
   },
   {
-    "path": "/api/signup",
-    "controller": "signupAPI",
-    "middlewares": []
+    path: "/api/signup",
+    controller: "signupAPI",
+    middlewares: []
   },
   {
-    "path": "/api/login",
-    "controller": "loginAPI",
-    "middlewares": [],
-    "params": {
-      "username": {
-        "type": "number",
-        "required": true,
-        "maxLength": 50,
-        "minLength": 20
+    path: "/api/login",
+    controller: "loginAPI",
+    middlewares: [],
+    params: {
+      username: {
+        type: "number",
+        required: true,
+        maxLength: 50,
+        minLength: 20
       },
-      "password": {
-        "type": "string",
-        "required": true
+      password: {
+        type: "string",
+        required: true
       }
     }
   }
--- src/core/_all.d.ts
+++ src/core/_all.d.ts
@@ -3,8 +3,22 @@ interface MongoDoc {
     [propName: string]: any;
 }
 
+
 interface SocotraResponse {
     error: number;
     msg?: string;
-    [propName: string]: any
+    [propName: string]: any;
+}
+
+
+interface SocotraRoute {
+    path: string;
+    controller: string;
+    method?: string;
+    middlewares?: Array<string>;
+    params?: any;
+}
+
+interface SocotraRouteParams {
+    [propName: string]: any;
 }
